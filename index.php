13c46d618e465d5d02817977fa31aa63a2f8a989
alexey129 <dr.alex3754@yandex.ru>
2022-01-30 22:55:09 +0700

    исправлен конфиг сайта 2

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
 require_once './config.php';
-$dbconn = pg_connect("host=localhost
+$dbconn = pg_connect("host=ec2-3-227-15-75.compute-1.amazonaws.com
 					  port=5432
-					  dbname=bs135
-					  user=postgres
-					  password=password
+					  dbname=da0mrntn88a3vi
+					  user=fnhbktcfomqyqc
+					  password=ef91e3b2d02efa2c0cc9f6c243462f03aa66e63965241c5f1b424d0efc54fa74
 					");
 
 $IsAuthentification = "";
