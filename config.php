8e3300b81b717f086448a8a97b4d854dbe3bff8a
alexey129 <dr.alex3754@yandex.ru>
2022-01-30 22:51:37 +0700

    исправлен конфиг сайта

--- config.php
+++ config.php
@@ -2,8 +2,14 @@
 define("SITE_PATH", "http://bs135/");
 
 //константы для базы данных
-define("HOST", "localhost");
+/*define("HOST", "localhost");
 define("PORT", "5432");
 define("DBNAME", "bs135");
 define("USER", "postgres");
-define("PASSWORD", "password");
+define("PASSWORD", "password");*/
+
+define("HOST", "ec2-3-227-15-75.compute-1.amazonaws.com");
+define("PORT", "5432");
+define("DBNAME", "da0mrntn88a3vi");
+define("USER", "fnhbktcfomqyqc");
+define("PASSWORD", "ef91e3b2d02efa2c0cc9f6c243462f03aa66e63965241c5f1b424d0efc54fa74");
